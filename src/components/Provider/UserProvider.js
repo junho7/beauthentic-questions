7f0f4ae207aa6425c8bee302c1885841c2241b87
Jun Ho Park <junho7@users.noreply.github.com>
2020-12-10 06:36:50 -0800

    Update UserProvider.js

--- src/components/Provider/UserProvider.js
+++ src/components/Provider/UserProvider.js
@@ -1,6 +1,3 @@
-// import React, { Component, createContext } from "react";
-// import { auth } from "../Firebase/firebase";
-
 import { createContext } from "react";
 export const AuthContext = createContext({
     isLoggedIn: false,
@@ -9,24 +6,3 @@ export const AuthContext = createContext({
     logout: () => {}
 });
 
-// export const UserContext = createContext({ user: null });
-
-// class UserProvider extends Component {
-//   state = {
-//     user: null
-//   };
-
-//   componentDidMount = () => {
-//     auth.onAuthStateChanged(userAuth => {
-//       this.setState({ user: userAuth});
-//     });
-//   };
-//   render() {
-//     return (
-//       <UserContext.Provider value={this.state.user}>
-//         {this.props.children}
-//       </UserContext.Provider>
-//     );
-//   }
-// }
-// export default UserProvider;
