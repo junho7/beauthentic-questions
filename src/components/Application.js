b76111a5453b9afb4e0801fe0f9cc1bb775e9b64
Jun Ho Park <junho7@users.noreply.github.com>
2020-12-09 21:14:33 -0800

    Update Application.js
    Removed unused code

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,32 +11,18 @@ import Myquestions from './Myquestions'
 import Createset from './Createset'
 import Modifyset from './Modifyset'
 import Slides from './Slides';
-// import { Redirect } from "react-router-dom";
 
 function Application() {
   const user = useContext(AuthContext);
   return (
-      //   user ?
-      //   <div>
-      //   <Home />
-      //   <ProfilePage />
-      //   </div>
-      // :
       <Router>
           <Home path = "/" />
           <SignUp path="signUp" />
           <SignIn path="signIn" /> 
-          {/* <Slides path="slides/:url" />  */}
-          {/* <Slides path="slides" />  */}
-          
-          {/* <Redirect from="slides/" to="slides/:questionset" /> 
-          <Redirect from="slides" to="slides/:questionset" />  */}
-          {/* <Slides path = "slides"> */}
             <Slides path="slides"> 
               <Slides path=":questionset" /> 
               </Slides>
             <Slides path="slides/" /> 
-          {/* </Slides> */}
           <Myquestions path="myquestions" /> 
           <Createset path="createset" /> 
           <Modifyset path="modifyset/:questionset" /> 
