9f6f545dcdcfc57acd840274974d0edcf3a01ca4
Jun Ho Park <junho7@users.noreply.github.com>
2020-12-10 06:17:17 -0800

    Update Myquestions.js
    Removed unused code

--- src/components/Myquestions.js
+++ src/components/Myquestions.js
@@ -1,43 +1,26 @@
 import React, { useContext }  from "react";
-// import SignUp from './SignUp'
-// import SignIn from './SignIn'
 import { getQuestionsetList } from "./Firebase/firebase";
 import ReactDOM from "react-dom";
-// import { Redirect } from 'react-router';
 import Slider from "react-slick";
 import Slides from './Slides';
 import Modifyset from './Modifyset';
 import ProfilePage from "./ProfilePage";
-// import { compose } from 'recompose';
-
-// import { withAuthorization, withEmailVerification } from './Session';
-// import Messages from './Messages';
-// import {
-//   Link
-// } from 'react-router-dom'
 import { Link } from "@reach/router";
-// import "./Home.css";
 import { AuthContext } from "./Provider/UserProvider";
 import { Form, Col, InputGroup, FormControl, Container, Button, Modal } from 'react-bootstrap';
 
-
-
-// export class Home extends React.Component {
 const Myquestions = () => {
-  // render() {
     const user = useContext(AuthContext);
 
     const [qlist, setQlist] = React.useState(['test', 'test1'])
 
     
     React.useEffect(() => {
-      console.log("qlist: ", qlist)
-    var qresult = getQuestionsetList().then((res)=>{console.log(res); setQlist(res)});
+    var qresult = getQuestionsetList().then((res)=>{setQlist(res)});
 
     },[]);
 
     const btnStyle = {
-      // color: "blue"
       "color": "#564D65",
       "backgroundColor": "#2CDA9D",
       "fontSize":"30px",
@@ -46,30 +29,18 @@ const Myquestions = () => {
       "fontWeight" : "bold"
       
     };
-    // const history = useHistory();
-    // const handleOnClick = (key) => history.push('modifyset'+key);
     const handleOnClick= (key) => {
-    // this.setState({redirect: true});
-    // this.props.history.push(`modifyset/${key}`)
-    // const user = useContext(UserContext);
     }
 
     function copyUrl(event) {
       event.preventDefault();
-      /* Get the text field */
-      // var copyText = document.getElementById("urlString");
       var copyText = document.getElementById(event.currentTarget.id);
     
-      /* Select the text field */
-      // console.log("copyText:", copyText);
-      console.log("copyText:", copyText);
       copyText.select();
       copyText.setSelectionRange(0, 99999); /*For mobile devices*/
     
-      /* Copy the text inside the text field */
       document.execCommand("copy");
-    
-      /* Alert the copied text */
+
       alert("Copied the text: " + copyText.value);
     }
 
@@ -85,7 +56,6 @@ const Myquestions = () => {
       <Link to={'/slides/'+key}>
       <InputGroup.Text>
 {value}
-{/* {value.questionsetname} */}
 </InputGroup.Text>
 </Link>
 </InputGroup.Prepend>
@@ -106,31 +76,10 @@ const Myquestions = () => {
 }
 
 
-      {/* {qlist.map((item, index) => (
-        // <Fragment key={`${inputField}~${index}`}>
-        // <Fragment>
-          <div>
-            <label>{index+1}</label>
-            <input
-              type="text"
-              // id={index}
-              // id={uuid()}
-              value={item}
-              // onChange={event => handleInputChange(index+1, event)}
-            />
-          </div>
-        // </Fragment>
-      ))} */}
       </Form>
       </Container>
     );
-  // }
 }
 
-// const condition = authUser => !!authUser;
+
 export default Myquestions;
-// ReactDOM.render(<Slide />, document.getElementById("container"));
-// export default compose(
-//   withEmailVerification,
-//   withAuthorization(condition),
-// )(Home);
