06a33e849cddd6dd699692ede2247303081f16c9
Jun Ho Park <junho7@users.noreply.github.com>
2020-12-09 21:13:14 -0800

    Update App.js
    Removed unused code

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,16 @@
 import React, {useState} from 'react';
 import Home from './Home'
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link
-// } from 'react-router-dom'
-// import * as ROUTES from '../constants/routes';
-// import logo from './logo.svg';
 import Slides from './Slides';
 import End from './End';
 import './App.css';
 import "react-bootstrap/dist/react-bootstrap.min.js";
 import Application from "./Application";
-// import UserProvider from "./Provider/UserProvider";
-// import { UserContext } from "./Provider/UserProvider";
 import { AuthContext } from "./Provider/UserProvider";
 import { useContext } from "react";
 import { Router } from "@reach/router";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import ProfilePage from "./ProfilePage";
-
-// const App = () => (
-//   <Router>
-//            <Route exact path = '/' component={Home} />      
-//            <Route path = '/slides' component={Slides} />
-//            <Route path = '/end' component={End} />
-//   </Router>
-// );
 
 function App() {
   const [isLoggedIn, setLoggedIn] = useState(false);
@@ -40,35 +23,9 @@ function App() {
 
   return (
     <AuthContext.Provider value= {{isLoggedIn: isLoggedIn, login: login, logout: logout}} >
-    {/* <div className="App">
-      <header className="App-header">
-        <Router>
-          <div className='appcenter'>
-          <Route exact path = '/' component={Home} />      
-          <Route path = '/slides' component={Slides} />
-          <Route path = '/end' component={End} />
-          </div>
-        </Router> */}
-        {/*
-          <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      {/* </header>
-    </div> */}
     <Application />
     </AuthContext.Provider>
   );
 }
-
-
 
 export default App;
